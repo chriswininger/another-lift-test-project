1010f4dcd0f24179aec8c498c19241af5d009def
Chris Wininger <cwininger@sonatype.com>
2022-11-04 16:35:18 -0400

    try again

--- src/main/java/com/mycompany/app/Lift4698.java
+++ src/main/java/com/mycompany/app/Lift4698.java
@@ -1,22 +1,12 @@
 package com.mycompany.app;
 
-import java.io.File;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
-import java.util.List;
-
-import org.apache.maven.model.building.ModelBuildingRequest;
-import org.apache.maven.model.building.ModelProblemCollector;
-import org.codehaus.plexus.interpolation.InterpolationPostProcessor;
-import org.codehaus.plexus.interpolation.ValueSource;
-import org.springframework.ui.Model;
 
 // test for an issue where xlint errors are treated as error prone findings
 public class Lift4698
 {
     public void test() {
-        AccessController.doPrivileged( action );
-
         System.out.println("much foo");
 
         Derived d = new Derived();
@@ -40,7 +30,7 @@ public class Lift4698
             public Object run() {
                 return null;
             }
-        }
+        };
 
         AccessController.doPrivileged(action );
     }
