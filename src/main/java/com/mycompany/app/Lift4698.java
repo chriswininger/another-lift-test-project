2e97f824306fa4c88ee71028f3c94f05075095b5
Chris Wininger <cwininger@sonatype.com>
2022-11-04 16:17:13 -0400

    more problems

--- src/main/java/com/mycompany/app/Lift4698.java
+++ src/main/java/com/mycompany/app/Lift4698.java
@@ -1,8 +1,22 @@
 package com.mycompany.app;
+
+import java.io.File;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.util.List;
+
+import org.apache.maven.model.building.ModelBuildingRequest;
+import org.apache.maven.model.building.ModelProblemCollector;
+import org.codehaus.plexus.interpolation.InterpolationPostProcessor;
+import org.codehaus.plexus.interpolation.ValueSource;
+import org.springframework.ui.Model;
+
 // test for an issue where xlint errors are treated as error prone findings
 public class Lift4698
 {
     public void test() {
+        AccessController.doPrivileged( action );
+
         System.out.println("much foo");
 
         Derived d = new Derived();
@@ -12,6 +26,23 @@ public class Lift4698
         Base c = (Base) new Base();
         System.out.println(b);
         System.out.println(c);
+    }
+
+    /*
+        this should trigger a compiler warning about removing deprecated calls which is causing an issue
+
+        https://github.com/chriswininger/maven/blob/a53a1aa232bc383baf055d884a7c66319d10d404/maven-model-builder/src/main/java/org/apache/maven/model/interpolation/StringSearchModelInterpolator.java#L94
+     */
+    void interpolateObject()
+    {
+        PrivilegedAction<Object> action = new PrivilegedAction<Object>() {
+            @Override
+            public Object run() {
+                return null;
+            }
+        }
+
+        AccessController.doPrivileged(action );
     }
 }
 
