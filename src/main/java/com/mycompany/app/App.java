eb70861b72b818ec20600e5989e76669c298d2e4
Chris Wininger <cwininger@sonatype.com>
2022-11-17 14:26:05 -0500

    make all tools fail

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,6 +4,8 @@ public class App {
   public static void main(String [] args) {
     System.out.println("Hello world");
 
+    var fooBar= not java
+
     Derived d = new Derived();
     Base b = (Base) d;
 
